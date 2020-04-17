001cc21f17ccae2ac200526ebd66693e474fe0a4
abmoRobotics <56405924+abmoRobotics@users.noreply.github.com>
2020-04-17 11:20:48 +0200

    sdf

--- Simulering/src/Tomato_example.cpp
+++ Simulering/src/Tomato_example.cpp
@@ -3,6 +3,15 @@
 #include "Melon.h"
 #include "graphics.h"
 #include <iostream>
+#include <math.h>
+
+#define PI 3.14159265
+
+double degtorad(double deg){
+    double rad =  (deg * PI) / 180.0 ;
+    return rad;
+}
+
 
 int main(int argc, char const *argv[])
 {
@@ -17,11 +26,11 @@ int main(int argc, char const *argv[])
 
     //Pushes pointer into greenhouse - respresenting tomatos and cucumbers
     float windowHeight = window.getSize().y;
-    greenhouse.push_back(std::make_shared<Tomato>(100.0F, windowHeight));
+    greenhouse.push_back(std::make_shared<Tomato>(335.0F, windowHeight-190));
     greenhouse.push_back(std::make_shared<Tomato>(200.0F, windowHeight));
     greenhouse.push_back(std::make_shared<Cucumber>(300.0F, windowHeight));
-    greenhouse.push_back(std::make_shared<Melon>(400.0F, windowHeight));
-    greenhouse.push_back(std::make_shared<Melon>(600.0F, windowHeight));
+    greenhouse.push_back(std::make_shared<Melon>(550.0F, windowHeight-150));
+    greenhouse.push_back(std::make_shared<Melon>(620.0F, windowHeight-190));
     greenhouse.push_back(std::make_shared<Melon>(800.0F, windowHeight));
     sf::Clock deltaClock;
     // run the program as long as the window is open
@@ -63,6 +72,108 @@ int main(int argc, char const *argv[])
         {
             greenhouse[i]->draw(window);
         }
+
+
+        int posx = 500;
+        int posy = 650;
+        float length = 300.0F;
+        float width = 150.0F;
+        float height = 100.0F;
+
+        sf::RectangleShape greenhouseDraw1;
+        greenhouseDraw1.setSize({5.0F,length});
+        greenhouseDraw1.setRotation(240);
+        greenhouseDraw1.setPosition(posx,posy);
+        window.draw(greenhouseDraw1);
+
+        sf::RectangleShape greenhouseDraw2;
+        greenhouseDraw2.setSize({5.0F,height});
+        greenhouseDraw2.setRotation(180);
+        greenhouseDraw2.setPosition(posx,posy);
+        window.draw(greenhouseDraw2);
+
+        sf::RectangleShape greenhouseDraw3;
+        greenhouseDraw3.setSize({5.0F,width*1.5F});
+        greenhouseDraw3.setRotation(120);
+        greenhouseDraw3.setPosition(posx,posy);
+        window.draw(greenhouseDraw3);
+
+        sf::RectangleShape greenhouseDraw4;
+        greenhouseDraw4.setSize({5.0F,length});
+        greenhouseDraw4.setRotation(240);
+        greenhouseDraw4.setPosition(posx,posy-100);
+        window.draw(greenhouseDraw4);
+
+        sf::RectangleShape greenhouseDraw5;
+        greenhouseDraw5.setSize({5.0F,width});
+        greenhouseDraw5.setRotation(120);
+        greenhouseDraw5.setPosition(posx,posy-100);
+        window.draw(greenhouseDraw5);
+
+        sf::RectangleShape greenhouseDraw6;
+        greenhouseDraw6.setSize({5.0F,height});
+        greenhouseDraw6.setRotation(180);
+        greenhouseDraw6.setPosition(posx+cos(degtorad(30))*length,posy-sin(degtorad(30))*length);
+        window.draw(greenhouseDraw6);
+
+        sf::RectangleShape greenhouseDraw7;
+        greenhouseDraw7.setSize({5.0F,150.0F});
+        greenhouseDraw7.setRotation(120);
+        greenhouseDraw7.setPosition(posx+cos(degtorad(30))*length,posy-sin(degtorad(30))*length-100);
+        window.draw(greenhouseDraw7);
+
+        sf::RectangleShape greenhouseDraw8;
+        greenhouseDraw8.setSize({5.0F,300.0F});
+        greenhouseDraw8.setRotation(180);
+        greenhouseDraw8.setPosition(posx-cos(degtorad(30))*width,posy-sin(degtorad(30))*width);
+        window.draw(greenhouseDraw8);
+
+
+        sf::RectangleShape greenhouseDraw9;
+        greenhouseDraw9.setSize({5.0F,length});
+        greenhouseDraw9.setRotation(240);
+        greenhouseDraw9.setPosition(posx-cos(degtorad(30))*width,posy-sin(degtorad(30))*width-100);
+        window.draw(greenhouseDraw9);
+
+        sf::RectangleShape greenhouseDraw10;
+        greenhouseDraw10.setSize({5.0F,300.0F});
+        greenhouseDraw10.setRotation(180);
+        greenhouseDraw10.setPosition(posx-cos(degtorad(30))*width*1.5,posy-sin(degtorad(30))*width*1.5);
+        window.draw(greenhouseDraw10);
+
+        sf::RectangleShape greenhouseDraw11;
+        greenhouseDraw11.setSize({5.0F,length});
+        greenhouseDraw11.setRotation(240);
+        greenhouseDraw11.setPosition(posx-cos(degtorad(30))*width,(posy-sin(degtorad(30))*width)-length);
+        window.draw(greenhouseDraw11);
+
+        sf::RectangleShape greenhouseDraw12;
+        greenhouseDraw12.setSize({5.0F,length});
+        greenhouseDraw12.setRotation(240);
+        greenhouseDraw12.setPosition(posx-cos(degtorad(30))*width*1.5,(posy-sin(degtorad(30))*width*1.5)-length);
+        window.draw(greenhouseDraw12);
+
+        sf::RectangleShape greenhouseDraw13;
+        greenhouseDraw13.setSize({5.0F,width*0.5F});
+        greenhouseDraw13.setRotation(120);
+        greenhouseDraw13.setPosition(posx-cos(degtorad(30))*width,(posy-sin(degtorad(30))*width)-length);
+        window.draw(greenhouseDraw13);
+
+        sf::RectangleShape greenhouseDraw14;
+        greenhouseDraw14.setSize({5.0F,width*0.5F});
+        greenhouseDraw14.setRotation(120);
+        greenhouseDraw14.setPosition((posx-cos(degtorad(30))*width)+cos(degtorad(30))*length,((posy-sin(degtorad(30))*width)-length)-sin(degtorad(30))*length);
+        window.draw(greenhouseDraw14);
+
+        sf::RectangleShape greenhouseDraw15;
+        greenhouseDraw15.setSize({5.0F,height*2});
+        greenhouseDraw15.setRotation(0);
+        greenhouseDraw15.setPosition((posx-cos(degtorad(30))*width)+cos(degtorad(30))*length,((posy-sin(degtorad(30))*width)-length)-sin(degtorad(30))*length);
+        window.draw(greenhouseDraw15);
+
+
+
+
 
         ImGui::SFML::Render(window);
         // end the current frame
